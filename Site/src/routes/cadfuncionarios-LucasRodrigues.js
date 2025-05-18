6dde09c76b9663f1398ff76ea0c3b2cdd2d7f6c1
Lucas Rodrigues <lucas.rodrigues127@sptech.school>
2025-05-18 15:59:22 -0300

    ajustes

--- Site/src/routes/cadfuncionarios-LucasRodrigues.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var personagemController = require('../controllers/personagemController');
-
-router.post('/cadastrar', personagemController.cadastrar);
-
-module.exports = router;
