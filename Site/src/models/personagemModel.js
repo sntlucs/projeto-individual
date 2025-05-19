5e0940d2e7f0696b72666f3f8222f3aade016dbf
lucas <lucas.rodrigues127@sptech.school>
2025-05-19 12:17:21 -0300

    Bug corrigido WebDataViz

--- Site/src/models/personagemModel.js
+++ Site/src/models/personagemModel.js
@@ -15,7 +15,7 @@ function listarPersonagem() {
     console.log("ACESSEI O PERSONAGEM MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarFuncionarios()");
 
     var instrucaoSql = `
-        SELECT id, nome, origem, classe, nivel FROM funcionario;
+        SELECT id, nome, origem, classe, nivel FROM Personagem;
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
