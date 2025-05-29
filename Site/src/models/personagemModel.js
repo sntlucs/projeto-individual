eba46c31c6a9bbca832d5b84a0586af4128d5026
lucas <lucas.rodrigues127@sptech.school>
2025-05-28 21:06:07 -0300

    redirecionamento no routes e correção de bugs

--- Site/src/models/personagemModel.js
+++ Site/src/models/personagemModel.js
@@ -1,4 +1,4 @@
-const db = require("../database/connection");
+const db = require("../database/config");
 
 function listar(callback) {
     const sql = `
--- Site/src/routes/personagem.js
+++ Site/src/routes/personagem.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/personagem");
+const controller = require("../controllers/personagemController");
 
-router.post("/", controller.criarPersonagem);
-router.get("/", controller.listarPersonagens);
-router.get("/:id", controller.buscarPersonagemPorId);
-router.put("/:id", controller.atualizarPersonagem);
-router.delete("/:id", controller.deletarPersonagem);
+router.post("/", controller.inserir);
+router.get("/", controller.listar);
+router.get("/:id", controller.buscarPorId);
+router.put("/:id", controller.atualizar);
+router.delete("/:id", controller.deletar);
 
 module.exports = router;
