9bd2ebda2f61914a025b8f06f6f0ea6f5d001d8c
lucas <lucas.rodrigues127@sptech.school>
2025-06-01 19:25:37 -0300

    ajustes no css gerenciador de personagem

--- Site/public/js/cadastro.js
+++ Site/public/js/cadastro.js
@@ -2,7 +2,6 @@ let listaUsuarios = [];
 
 function cadastrar() {
 
-
   var nomeVar = ipt_nome.value;
   var emailVar = ipt_email.value;
   var senhaVar = ipt_senha.value;
@@ -13,51 +12,51 @@ function cadastrar() {
     senhaVar == ""
 
   ) {
-    finalizarAguardar("Os campos não podem ser vazios.");
+    alert("Os campos não podem ser vazios.");
     return false;
   }
 
   if (nomeVar.length <= 1) {
-    finalizarAguardar("O nome deve conter mais de 1 caractere.");
-    return false;
+    alert("O nome deve conter mais de 1 caractere.");
+    return ;
   }
 
   if (!emailVar.includes('@') || !emailVar.includes('.')) {
-    finalizarAguardar("O e-mail é inválido.");
-    return false;
+    alert("O e-mail é inválido.");
+    return ;
   }
 
-  fetch("/usuario/cadastrar", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      nomeServer: nomeVar,
-      emailServer: emailVar,
-      senhaServer: senhaVar
-    }),
-  })
-    .then(function (resposta) {
-      console.log("resposta: ", resposta);
-
-      if (resposta.ok) {
-        var sectionErrosLogin = document.getElementById("section_erros_login");
-        sectionErrosLogin.style.backgroundColor = '#069006';
-      
-        finalizarAguardar("Cadastro realizado com sucesso! Redirecionando para tela de login...");
-
-        setTimeout(() => {
-          window.location = "login.html";
-        }, "2000");
-      } else {
-        throw "Houve um erro ao tentar realizar o cadastro!";
-      }
+    fetch("/usuario/cadastrar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nomeServer: nomeVar,
+        emailServer: emailVar,
+        senhaServer: senhaVar,
+      }),
     })
-    .catch(function (resposta) {
-      console.log(`#ERRO: ${resposta}`);
-      finalizarAguardar(resposta);
-    });
+      .then(function (resposta) {
+        console.log("resposta: ", resposta);
+
+        if (resposta.ok) {
+
+          setTimeout(() => {
+            window.location = "login.html";
+          }, "1000");
+
+        } else {
+          alert('Esse e-mail já foi cadastrado!')
+          throw "Houve um erro ao tentar realizar o cadastro!";
+          
+        }
+      })
+      .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+      });
+
+    return false;
 }
 
 function sumirMensagem() {
